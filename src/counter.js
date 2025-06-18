cb4837f9496a5c77411b5e1552f53713faeaada5
Deek2G <e.nisperos.535011@umindanao.edu.ph>
2025-06-18 16:56:45 +0800

    minor changes pt. 3

--- src/counter.js
+++ src/counter.js
@@ -1,12 +1,1 @@
-// Simple counter module for demo purposes
-export function setupCounter(element) {
-  let count = 0;
-  const update = () => {
-    element.innerText = `Count: ${count}`;
-  };
-  element.addEventListener('click', () => {
-    count++;
-    update();
-  });
-  update();
-}
+// Counter demo code removed. No longer used in production.
